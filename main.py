57bfcf5f6a78090b61e1ba3c55c09070c7844a4e
MONFOUGA MARIE <marie.monfouga@gmail.com>
2020-12-09 17:49:47 +0100

    changed from global to main function

--- main.py
+++ main.py
@@ -25,35 +25,38 @@ def row_to_ASCII_line(row):
         res += gray_to_ascii(pixel)
     return res
 
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3,SIZE) #width
+    cap.set(4,SIZE) #height
+    i=0
 
-cap = cv2.VideoCapture(0)
-cap.set(3,SIZE) #width
-cap.set(4,SIZE) #height
-i=0
+    if cap.isOpened():
+        ret, frame = cap.read()
+    else:
+        ret = False
 
-if cap.isOpened():
-    ret, frame = cap.read()
-else:
-    ret = False
+    while(ret):
+        # Capture frame by frame
+        ret, frame = cap.read()
 
-while(ret):
-    # Capture frame by frame
-    ret, frame = cap.read()
+        # Array of gray value
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Array of gray value
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        text = ""
+        for row in gray:
+            text += row_to_ASCII_line(row) + '\n'
+        print(text + f'\033[{SIZE+1}A\033[K')
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # Display the resulting frame
-    text = ""
-    for row in gray:
-        text += row_to_ASCII_line(row) + '\n'
-    print(text + f'\033[{SIZE+1}A\033[K')
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.waitKey(33)
 
-    cv2.waitKey(33)
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __package__ == "__main__":
+    main()
