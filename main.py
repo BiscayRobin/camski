a91ebe645d6549e602aec39be4e234e56df848d3
Robin Biscay <Biscay.rob@gmail.com>
2021-01-25 13:47:42 +0100

    better formating

--- main.py
+++ main.py
@@ -2,21 +2,21 @@
 
 import cv2
 
-ASCII = [
-    '@','%','#','*','+','=','-',':','.',' '
-]
+ASCII = ['@', '%', '#', '*', '+', '=', '-', ':', '.', ' ']
 
 SIZE = 256
+
 
 def map_range(src, dst, value):
     src_start, src_end = src
     dst_start, dst_end = dst
-    return dst_start + ((value - src_start) * (dst_end - dst_start)) / (src_end - src_start)
+    return dst_start + ((value - src_start) *
+                        (dst_end - dst_start)) / (src_end - src_start)
+
 
 def gray_to_ascii(value):
-    return ASCII[
-            len(ASCII) - 1 - min(int(value * len(ASCII) / 255),            len(ASCII)-1)
-    ]
+    return ASCII[len(ASCII) - 1 - min(int(value * len(ASCII) / 255),
+                                      len(ASCII) - 1)]
 
 
 def row_to_ASCII_line(row):
@@ -25,18 +25,18 @@ def row_to_ASCII_line(row):
         res += gray_to_ascii(pixel)
     return res
 
+
 def main():
     cap = cv2.VideoCapture(0)
-    cap.set(3,SIZE) #width
-    cap.set(4,SIZE) #height
-    i=0
+    cap.set(3, SIZE)  # width
+    cap.set(4, SIZE)  # height
 
     if cap.isOpened():
         ret, frame = cap.read()
     else:
         ret = False
 
-    while(ret):
+    while (ret):
         # Capture frame by frame
         ret, frame = cap.read()
 
@@ -51,7 +51,7 @@ def main():
                 text += row_to_ASCII_line(row) + '\n'
             line_jumper += 1
         print(text + f'\033[{SIZE+1}A\033[K')
-        cv2.imshow('frame',gray)
+        cv2.imshow('frame', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -60,6 +60,7 @@ def main():
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
